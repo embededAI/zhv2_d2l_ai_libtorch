da0fd050cf519150495698b63e3661f112f93a65
Liu Hang <liuhang@LiudeMac>
2021-04-02 00:23:18 +0800

    update content

--- inc/load_opencv.hpp
+++ inc/load_opencv.hpp
@@ -20,12 +20,13 @@
 #ifndef LOAD_OPENCV_HPP
 #define LOAD_OPENCV_HPP
 
-#pragma cling add_library_path("/usr/local/lib")
-#pragma cling add_include_path("/usr/local/include")
-#pragma cling add_include_path("/usr/local/include/opencv4")
 
 
 #if defined(__linux__)
+
+#pragma cling add_library_path("/usr/local/lib")
+#pragma cling add_include_path("/usr/local/include")
+#pragma cling add_include_path("/usr/local/include/opencv4")
 
 #pragma cling load("libopencv_imgproc.so")
 #pragma cling load("libopencv_highgui.so")
@@ -80,6 +81,59 @@
 
 #elif __APPLE__
 
+#pragma cling add_library_path("/usr/local/Cellar/opencv/4.5.1_2/lib")
+#pragma cling add_include_path("/usr/local/Cellar/opencv/4.5.1_2/include/opencv4")
+#pragma cling add_include_path("/usr/local/Cellar/opencv/4.5.1_2/include")
+
+#pragma cling load("libopencv_imgproc.dylib")
+#pragma cling load("libopencv_highgui.dylib")
+#pragma cling load("libopencv_videoio.dylib")
+#pragma cling load("libopencv_video.dylib")
+#pragma cling load("libopencv_imgcodecs.dylib")
+#pragma cling load("libopencv_features2d.dylib")
+
+#pragma cling load("libopencv_gapi.dylib") 
+#pragma cling load("libopencv_stitching.dylib")
+#pragma cling load("libopencv_aruco.dylib")
+#pragma cling load("libopencv_bgsegm.dylib")
+#pragma cling load("libopencv_bioinspired.dylib")
+#pragma cling load("libopencv_ccalib.dylib")
+#pragma cling load("libopencv_dnn_objdetect.dylib")
+#pragma cling load("libopencv_dnn_superres.dylib")
+#pragma cling load("libopencv_dpm.dylib")
+#pragma cling load("libopencv_face.dylib")
+#pragma cling load("libopencv_freetype.dylib")
+#pragma cling load("libopencv_fuzzy.dylib")
+// #pragma cling load("libopencv_hdf.dylib")
+#pragma cling load("libopencv_hfs.dylib")
+#pragma cling load("libopencv_img_hash.dylib")
+#pragma cling load("libopencv_line_descriptor.dylib")
+#pragma cling load("libopencv_quality.dylib")
+#pragma cling load("libopencv_reg.dylib")
+#pragma cling load("libopencv_rgbd.dylib")
+#pragma cling load("libopencv_saliency.dylib")
+#pragma cling load("libopencv_stereo.dylib")
+#pragma cling load("libopencv_structured_light.dylib")
+#pragma cling load("libopencv_phase_unwrapping.dylib")
+#pragma cling load("libopencv_superres.dylib")
+#pragma cling load("libopencv_optflow.dylib")
+#pragma cling load("libopencv_surface_matching.dylib")
+#pragma cling load("libopencv_tracking.dylib")
+#pragma cling load("libopencv_datasets.dylib")
+#pragma cling load("libopencv_text.dylib")
+#pragma cling load("libopencv_dnn.dylib")
+#pragma cling load("libopencv_plot.dylib")
+#pragma cling load("libopencv_videostab.dylib")
+#pragma cling load("libopencv_xfeatures2d.dylib")
+#pragma cling load("libopencv_shape.dylib")
+#pragma cling load("libopencv_ml.dylib")
+#pragma cling load("libopencv_ximgproc.dylib")
+#pragma cling load("libopencv_xobjdetect.dylib")
+#pragma cling load("libopencv_objdetect.dylib")
+#pragma cling load("libopencv_calib3d.dylib")
+#pragma cling load("libopencv_flann.dylib")
+#pragma cling load("libopencv_xphoto.dylib")
+#pragma cling load("libopencv_photo.dylib")
 
 #endif
 
